20b8b89f420c2f6b7b44d912c0ea232275538418
Libby Cosedge <libby.cosedge@lobsterpictures.tv>
2019-12-17 17:26:32 +0000

    bit more settering and getting

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from room import Room
 
 
 kitchen = Room("kitchen")
+kitchen.set_description("A dank and dirty room buzzing with flies")
--- room.py
+++ room.py
@@ -2,3 +2,15 @@ class Room;
     def __init__(self, room_name):
         self.name = room_name
         self.description = None
+
+    def set_description(self, description):
+        self.description = set_description
+
+    def get_description(self):
+        return self.description
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
